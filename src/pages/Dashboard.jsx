6d4dcf888fb232521ad8709a1e08be8031c19228
Akash <hafejmohammadakash@gmail.com>
2025-04-24 01:36:35 +0600

    dasshboard a localsorage theke data anlsam

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getCoffeeCart } from "../utils/localStorage";
+import Card from "../components/Card";
 
 const Dashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [coffees, setCoffees] = useState([]);
+
+  useEffect(() => {
+    const favorite = getCoffeeCart();
+    setCoffees(favorite);
+  }, []);
+
+  return (
+    <div className="my-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {coffees.map((coffee) => (
+          <Card key={coffee.id} coffee={coffee} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
